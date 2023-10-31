622b13a674c1b1948cf05a2d65d9cf3e3dc46c1c
Ank1r0 <jenerieshope@gmail.com>
2023-10-31 19:58:25 +0100

    Chapter 8 Done

--- src/Content.js
+++ src/Content.js
@@ -1,34 +1,17 @@
-import { useState } from 'react'
+import ItemList from './ItemList';
 
-const Content = () => {
-
-    const [name, setName] = useState('Dave');
-    const handleNameChange = () => {
-        const names = ['Bob','Kevin','Dave']
-        const int = Math.floor(Math.random() * 3);
-        return names[int];
-    }
-
-    const handleClick = () => {
-      console.log('you clicked it')
-    }
-
-    const handleClick2 = (name) => {
-      console.log(`${name} was clicked`)
-    }
-
-    const handleClick3 = (e) => {
-      console.log(e.target.innerText)
-    }
-
+const Content = ({items,handleCheck,handleDelete}) => {
   return (
     <main>
-        <p onDoubleClick={handleClick}>
-            Hello {handleNameChange()}!
-        </p>
-        <button onClick={handleClick}>Click it</button>
-        <button onClick={() => handleClick2('Dave')}>Click it</button>
-        <button onClick={(e) => handleClick3(e)}>Click it</button>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+          />
+        ) : (
+          <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )}   
     </main>
   )
 }
